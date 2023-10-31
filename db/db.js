7707bf70399740930c1dd73e8fc228c413fbf1ae
SavindiLayanga <savindilayanga@gmail.com>
2023-10-31 21:08:56 +0530

    color

--- db/db.js
+++ db/db.js
@@ -1,8 +1,8 @@
 //store data
 var customerDB = [
-    {id: "C00-001", name: "Anu Dil", address: "Matara", salary: 100000},
-    {id: "C00-002", name: "Inu Jith", address: "Galle", salary: 100000},
-    {id: "C00-003", name: "Thenu Lith", address: "Galle", salary: 200000}
+    {id: "C00-001", name: "Savindi Layanga", address: "Matara", salary: 100000},
+    {id: "C00-002", name: "Anuki Dilara", address: "Galle", salary: 100000},
+    {id: "C00-003", name: "Dinithi Chethana", address: "Galle", salary: 200000}
 ];
 
 var itemDB = [
